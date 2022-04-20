3abb024a2fb9da7a05c38c6ed063e04c5e0c1c90
Sergei Krainyukov <sk-new@mail.ru>
2022-04-21 00:56:44 +0300

    changes

--- First.java
+++ First.java
@@ -2,8 +2,9 @@ public class First {
     public static int squirrel(int N) {
         int sum = N;
         for (int i = N - 1; i >= 1; i--) {
-            sum *= i;
+            sum *= N;
         }
-        return sum;
+        if (sum < 0) sum = sum * -1;
+        return Integer.parseInt(String.valueOf(Integer.toString(sum).toCharArray()[0]));
     }
 }
