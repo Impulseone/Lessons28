2ee3c010e5804aaab5cf79817e3bef7311de1e63
Sergei Krainyukov <sk-new@mail.ru>
2022-04-26 12:32:22 +0300

    refactor with simple pair class

--- Task6/Level1.java
+++ Task6/Level1.java
@@ -32,5 +32,31 @@ public class Level1 {
         return list;
     }
 
-    private record Pair(int a, int b) {}
+    private class Pair {
+        private final int a;
+        private final int b;
+
+        public Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (a != pair.a) return false;
+            return b == pair.b;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = a;
+            result = 31 * result + b;
+            return result;
+        }
+    }
 }
