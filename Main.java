e1be86f972c0f97691646725573f6a0c56830d5f
Sergei Krainyukov <sk-new@mail.ru>
2022-04-26 12:16:49 +0300

    task 6 upd

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import Task6.Level1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Level1.PatternUnlock(10, new int[]{1, 2, 3, 4, 5, 6, 2, 7, 8, 9}));
+        System.out.println(Level1.PatternUnlock(3, new int[]{2, 1, 9}));
     }
 }
--- Task6/Level1.java
+++ Task6/Level1.java
@@ -1,12 +1,36 @@
 public class Level1 {
     public static String PatternUnlock(int N, int[] hits) {
+        List<Pair> pairs = shortPathsList();
         double sum = 0;
         for (int i = 0; i < N - 1; i++) {
             int a = hits[i];
             int b = hits[i + 1];
-            if (b - a == 1) sum += 1;
+            if (b - a == 1 || a - b == 1 || pairs.contains(new Pair(a, b))) sum += 1;
             else sum += Math.sqrt(2);
         }
-        return String.format("%.5f", sum).replaceAll("\\D", "").replaceAll("0","");
+        return String.format("%.5f", sum).replaceAll("\\D", "").replaceAll("0", "");
     }
+
+    private static List<Pair> shortPathsList() {
+
+        ArrayList<Pair> list = new ArrayList<>();
+        list.add(new Pair(6, 1));
+        list.add(new Pair(1, 6));
+
+        list.add(new Pair(1, 9));
+        list.add(new Pair(9, 1));
+
+        list.add(new Pair(5, 2));
+        list.add(new Pair(2, 5));
+
+        list.add(new Pair(2, 8));
+        list.add(new Pair(8, 2));
+
+        list.add(new Pair(3, 7));
+        list.add(new Pair(7, 3));
+
+        return list;
+    }
+
+    private record Pair(int a, int b) {}
 }
